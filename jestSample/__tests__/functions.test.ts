9c78d09753044a4c74999397d47f68f03a60ec43
古川広一 <ex.kouichi1224@gmail.com>
2021-02-13 15:40:51 +0900

    Fix: テスト文章とexpect,actual の順番を修正

--- jestSample/__tests__/functions.test.ts
+++ jestSample/__tests__/functions.test.ts
@@ -13,27 +13,35 @@ describe("sumOfArray関数のテスト", (): void => {
   // });
 
   test("引数に空配列[]を渡すと0が返ってくる", (): void => {
+    const expected = 0;
+
     const actual = sumOfArray([]);
 
-    expect(0).toBe(actual);
+    expect(actual).toBe(expected);
   });
 
   test("引数に配列[1]を渡すと1が返ってくる", (): void => {
+    const expected = 1;
+
     const actual = sumOfArray([1]);
 
-    expect(1).toBe(actual);
+    expect(actual).toBe(expected);
   });
 
   test("引数に配列[1,2]を渡すと3が返ってくる", (): void => {
-    const actual: number = sumOfArray([1, 2]);
+    const expected = 3;
 
-    expect(3).toBe(actual);
+    const actual = sumOfArray([1, 2]);
+
+    expect(actual).toBe(expected);
   });
 
   test("引数に配列[-1,2]を渡すと1が返ってくる", (): void => {
-    const actual: number = sumOfArray([-1, 2]);
+    const expected = 1;
 
-    expect(1).toBe(actual);
+    const actual = sumOfArray([-1, 2]);
+
+    expect(actual).toBe(expected);
   });
 });
 
@@ -45,27 +53,27 @@ describe("asyncSumOfArray関数のテスト", (): void => {
   // });
 
   test("引数に空配列[]を渡すと0が返ってくる", async (): Promise<void> => {
+    const expected = 0;
+
     const actual = await asyncSumOfArray([]);
 
-    expect(0).toBe(actual);
+    expect(actual).toBe(expected);
   });
 
   test("引数に配列[1]を渡すと1が返ってくる", async (): Promise<void> => {
-    const actual: number = await asyncSumOfArray([1]);
+    const expected = 1;
 
-    expect(1).toBe(actual);
+    const actual = await asyncSumOfArray([1]);
+
+    expect(actual).toBe(expected);
   });
 
   test("引数に配列[-1,2]を渡すと1が返ってくる", async (): Promise<void> => {
-    const actual: number = await asyncSumOfArray([-1, 2]);
+    const expected = 1;
 
-    expect(1).toBe(actual);
-  });
+    const actual = await asyncSumOfArray([-1, 2]);
 
-  test("引数に配列[-1,1]を渡すと0が返ってくる", async (): Promise<void> => {
-    const actual: number = await asyncSumOfArray([-1, 1]);
-
-    expect(0).toBe(actual);
+    expect(actual).toBe(expected);
   });
 });
 
@@ -85,50 +93,43 @@ describe("asyncSumOfArraySometimesZero関数のテスト", (): void => {
   // });
 
   test("引数に空配列[]を渡すと0が返ってくる", async (): Promise<void> => {
+    const expected = 0;
     database.save = jest.fn();
 
-    const actual: number = await asyncSumOfArraySometimesZero(database, []);
+    const actual = await asyncSumOfArraySometimesZero(database, []);
 
-    expect(0).toBe(actual);
+    expect(actual).toBe(expected);
   });
 
   test("引数に配列[1]を渡すと1が返ってくる", async (): Promise<void> => {
+    const expected = 1;
     database.save = jest.fn();
 
-    const actual: number = await asyncSumOfArraySometimesZero(database, [1]);
+    const actual = await asyncSumOfArraySometimesZero(database, [1]);
 
-    expect(1).toBe(actual);
-  });
-
-  test("引数に配列[1,2]を渡すと3が返ってくる", async (): Promise<void> => {
-    database.save = jest.fn();
-
-    const actual: number = await asyncSumOfArraySometimesZero(database, [1, 2]);
-
-    expect(3).toBe(actual);
+    expect(actual).toBe(expected);
   });
 
   test("引数に配列[-1,2]を渡すと1が返ってくる", async (): Promise<void> => {
+    const expected = 1;
     database.save = jest.fn();
 
-    const actual: number = await asyncSumOfArraySometimesZero(database, [
-      -1,
-      2,
-    ]);
+    const actual = await asyncSumOfArraySometimesZero(database, [-1, 2]);
 
-    expect(1).toBe(actual);
+    expect(actual).toBe(expected);
   });
 
   test("DatabaseMock.save()が失敗すると0が返る", async (): Promise<void> => {
+    const expected = 0;
     database.save = jest.fn(
       (): Error => {
         throw new Error("fail!");
       }
     );
 
-    const actual: number = await asyncSumOfArraySometimesZero(database, [0]);
+    const actual = await asyncSumOfArraySometimesZero(database, [0]);
 
-    expect(0).toBe(actual);
+    expect(actual).toBe(expected);
   });
 });
 
@@ -139,36 +140,42 @@ describe("getFirstNameThrowIfLong関数のテスト", (): void => {
     nameApiSerivce = new NameApiService();
   });
 
-  test("firstNameが8文字の場合、引数に10を渡すとfirstNameが返ってくる", async (): Promise<
-    void
-  > => {
-    const firstName = "HOGEHOGE";
-    nameApiSerivce.getFirstName = jest.fn().mockResolvedValue(firstName);
+  describe("firstName.length <= maxNameLength の場合、firstName を返す", (): void => {
+    test("firstName = 'HOGEHOGE' , maxNameLength = 10 の場合、firstName を返す", async (): Promise<
+      void
+    > => {
+      const firstName = "HOGEHOGE";
+      const expected = firstName;
+      nameApiSerivce.getFirstName = jest.fn().mockResolvedValue(firstName);
 
-    const actual = await getFirstNameThrowIfLong(nameApiSerivce, 10);
+      const actual = await getFirstNameThrowIfLong(nameApiSerivce, 10);
 
-    expect(firstName).toBe(actual);
+      expect(actual).toBe(expected);
+    });
+
+    test("firstName = 'HOGEHOGE' , maxNameLength = 8 の場合、firstName を返す", async (): Promise<
+      void
+    > => {
+      const firstName = "HOGEHOGE";
+      const expected = firstName;
+      nameApiSerivce.getFirstName = jest.fn().mockResolvedValue(firstName);
+
+      const actual = await getFirstNameThrowIfLong(nameApiSerivce, 8);
+
+      expect(actual).toBe(expected);
+    });
   });
 
-  test("firstNameが8文字の場合、引数に8を渡すとfirstNameが返ってくる", async (): Promise<
-    void
-  > => {
-    const firstName = "HOGEHOGE";
-    nameApiSerivce.getFirstName = jest.fn().mockResolvedValue(firstName);
+  describe("firstName.length > maxNameLength の場合、エラーを返す", (): void => {
+    test("firstName = 'HOGEHOGE' , maxNameLength = 7 の場合、エラーを返す", async (): Promise<
+      void
+    > => {
+      const firstName = "HOGEHOGE";
+      nameApiSerivce.getFirstName = jest.fn().mockResolvedValue(firstName);
 
-    const actual = await getFirstNameThrowIfLong(nameApiSerivce, 10);
-
-    expect(firstName).toBe(actual);
-  });
-
-  test("firstNameが8文字の場合、引数に7を渡すとエラーが発生する", async (): Promise<
-    void
-  > => {
-    const firstName = "HOGEHOGE";
-    nameApiSerivce.getFirstName = jest.fn().mockResolvedValue(firstName);
-
-    await expect(getFirstNameThrowIfLong(nameApiSerivce, 7)).rejects.toThrow(
-      "first_name too long"
-    );
+      await expect(getFirstNameThrowIfLong(nameApiSerivce, 7)).rejects.toThrow(
+        "first_name too long"
+      );
+    });
   });
 });
