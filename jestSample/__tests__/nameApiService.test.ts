f06354ae13e820546d681e36c5e5482aa97089c6
古川広一 <ex.kouichi1224@gmail.com>
2021-02-13 16:28:45 +0900

    Fix: テストの文章を修正

--- jestSample/__tests__/nameApiService.test.ts
+++ jestSample/__tests__/nameApiService.test.ts
@@ -19,35 +19,54 @@ describe("NameApiServiceクラスのテスト", (): void => {
     nameApiService = new NameApiService();
   });
 
-  test("getFirstName関数が文字列hogeを返す場合、テストは成功する", async (): Promise<
-    void
-  > => {
-    const data: Response = {
-      /*eslint @typescript-eslint/camelcase: ["error", {properties: "never"}]*/
-      data: {
-        first_name: "hoge",
-      },
-    };
-    mockedAxios.get.mockResolvedValue(data);
-    const expected = "hoge";
+  describe("API から取得した firstName が 4 文字以下の場合、firstName を返す", (): void => {
+    test("firstName = 'hoge' の場合、'hoge' を返す", async (): Promise<
+      void
+    > => {
+      const data: Response = {
+        /*eslint @typescript-eslint/camelcase: ["error", {properties: "never"}]*/
+        data: {
+          first_name: "hoge",
+        },
+      };
+      mockedAxios.get.mockResolvedValue(data);
+      const expected = "hoge";
 
-    const actual = await nameApiService.getFirstName();
+      const actual = await nameApiService.getFirstName();
 
-    expect(actual).toBe(expected);
+      expect(actual).toBe(expected);
+    });
+
+    test("firstName = 'hog' の場合 'hog' を返す", async (): Promise<void> => {
+      const data: Response = {
+        /*eslint @typescript-eslint/camelcase: ["error", {properties: "never"}]*/
+        data: {
+          first_name: "hog",
+        },
+      };
+      mockedAxios.get.mockResolvedValue(data);
+      const expected = "hog";
+
+      const actual = await nameApiService.getFirstName();
+
+      expect(actual).toBe(expected);
+    });
   });
 
-  test("getFirstName関数が文字列hogehogeを返す場合、テストは失敗する", async (): Promise<
-    void
-  > => {
-    const data: Response = {
-      /*eslint @typescript-eslint/camelcase: ["error", {properties: "never"}]*/
-      data: {
-        first_name: "hogehoge",
-      },
-    };
-    mockedAxios.get.mockResolvedValue(data);
-    const expected = "firstName is too long!";
+  describe("API から取得した firstName が 5 文字以上の場合、エラーを返す", (): void => {
+    test("firstName = 'hogeh' の場合、エラーを返す", async (): Promise<
+      void
+    > => {
+      const data: Response = {
+        /*eslint @typescript-eslint/camelcase: ["error", {properties: "never"}]*/
+        data: {
+          first_name: "hogeh",
+        },
+      };
+      mockedAxios.get.mockResolvedValue(data);
+      const expected = "firstName is too long!";
 
-    await expect(nameApiService.getFirstName()).rejects.toThrow(expected);
+      await expect(nameApiService.getFirstName()).rejects.toThrow(expected);
+    });
   });
 });
